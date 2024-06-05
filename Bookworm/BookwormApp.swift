eab6a139987c98179d4593aa1930b2c6f12444fb
Álvaro Gascón <alvarogascon@hotmail.com>
2024-06-05 16:00:06 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Álvaro Gascón on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
